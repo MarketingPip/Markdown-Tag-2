ad9d8f6341e4991a5556282a99f3d21a67879fff
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-09-12 12:45:12 -0400

    Updated Markdown Tag :pencil:

--- src/markdown-tag.js
+++ src/markdown-tag.js
@@ -4,162 +4,253 @@
  * MARKDOWN FLAVOUR: STANDARD FLAVORED MARKDOWN. 
  * MORE INFO / FLAVOR OPTIONS CAN BE FOUND AT https://github.com/MarketingPipeline/Markdown-Tag/
  */
-var Debug = false;
-function renderMarkdown(){
-   
-/* Add Github CSS  */  
-  
-var CSSAdded = false;
-function addCss(fileName) {
 
-  if (CSSAdded == false){
-      var head = document.head;
-  var link = document.createElement("link");
 
-  link.type = "text/css";
-  link.rel = "stylesheet";
-  link.href = fileName;
 
-  head.appendChild(link);
-  CSSAdded = true
-   
-  } 
+// To Enable Debug Messages - set this to true
+var DebugMarkdownTag = false;
 
+/* Console Log Debbuger */
+function DEBUG(msg) {
+	if (DebugMarkdownTag === true) {
+		console.log(msg)
+	}
 }
 
-/* Convert Markdown Attributes */ 
-
-  
- 
-if (document.querySelectorAll('[md]').length > 0) {
-  
-  var converter = new showdown.Converter() 
-converter.setOption('tables', 'on')
-			
-converter.setOption('emoji', 'on')
-
-converter.setOption('strikethrough', 'on');
-  
-MD_TAG = document.querySelectorAll('[md]');
-for(var i=0; i<MD_TAG.length; i++)
-	  /// Temp fix for Gitub Block Quotes
-MD_TAG[i].innerHTML = converter.makeHtml(MD_TAG[i].innerHTML.replace(/&gt;/g, '>'))
-}
-
-  
-
-if (document.querySelectorAll('[github-md]').length > 0) {
-  
-addCss('https://cdn.jsdelivr.net/gh/MarketingPipeline/Markdown-Tag/stylesheets/github_md.min.css');
- 
-
-  var converter = new showdown.Converter() 
-  
-converter.setOption('tables', 'on')
-			
-converter.setOption('emoji', 'on')
-
-converter.setOption('strikethrough', 'on')
-
-converter.setOption('tasklists', 'true')
+/* Fetch MD files from URL or Path */
+async function getData(src) {
+	try {
+		DEBUG("Fetching File From URL or Path")
+		const res = await fetch(src);
+		const jsonResult = await res.text();
+		if (jsonResult === "Not Found") {
+			DEBUG("Error rendering content -  file path was not found")
+			throw 'Error rendering markdown contents - file Path Not Found';
+		} else {
+			DEBUG("File was loaded successfully")
+			return jsonResult
+		}
 
 
-converter.setOption('ghMentions', 'true')
-
-
-converter.setOption('simplifiedAutoLink', 'true');
-
-  
-  
-GitHub_MD_TAG = document.querySelectorAll('[github-md]');
-for(var i=0; i<GitHub_MD_TAG.length; i++)
-  /// Temp fix for Gitub Block Quotes
-   GitHub_MD_TAG = GitHub_MD_TAG[i]
-  GitHub_MD_TAG.classList.add("github-md");
-GitHub_MD_TAG.innerHTML = converter.makeHtml(GitHub_MD_TAG.innerHTML.replace(/&gt;/g, '>'))
-
-}  
-  
-
- /* Convert Markdown Tags */ 
-  
-if (document.getElementsByTagName("md").length > 0) {
-  
-  var converter = new showdown.Converter() 
-converter.setOption('tables', 'on')
-			
-converter.setOption('emoji', 'on')
-
-converter.setOption('strikethrough', 'on');
-  
-MD_TAG = document.getElementsByTagName("md");
-for(var i=0; i<MD_TAG.length; i++)
-	  /// Temp fix for Gitub Block Quotes
-MD_TAG[i].innerHTML = converter.makeHtml(MD_TAG[i].innerHTML.replace(/&gt;/g, '>'))
-}
-
-
-if (document.getElementsByTagName("github-md").length > 0) {
-  
-addCss('https://cdn.jsdelivr.net/gh/MarketingPipeline/Markdown-Tag/stylesheets/github_md.min.css');
- 
-
-  var converter = new showdown.Converter() 
-  
-converter.setOption('tables', 'on')
-			
-converter.setOption('emoji', 'on')
-
-converter.setOption('strikethrough', 'on')
-
-converter.setOption('tasklists', 'true')
-
-
-converter.setOption('ghMentions', 'true')
-
-
-converter.setOption('simplifiedAutoLink', 'true');
-
-  
-  
-GitHub_MD_TAG = document.getElementsByTagName("github-md");
-for(var i=0; i<GitHub_MD_TAG.length; i++)
-  /// Temp fix for Gitub Block Quotes
-GitHub_MD_TAG[i].innerHTML = converter.makeHtml(GitHub_MD_TAG[i].innerHTML.replace(/&gt;/g, '>'))
-
-}
+	} catch (error) {
+		return error;
+	}
 }
 
 
 
 
 
-function loadMarkdownParser(){
-  
-  /// Add Markdown Parser To Document
-var script = document.createElement('script');
-script.src = "https://cdn.jsdelivr.net/gh/MarketingPipeline/Markdown-Tag/parsers/showdown.min.js";
 
-document.head.appendChild(script); //or something of the likes  
-  
-  
-  
-  
-  // On Error Loading Markdown Parser
-script.onerror = function () {
- 
-  console.error("Markdown Tag: Error while performing function LoadMarkdownParser - There was an error loading the Markdown Parser")
-  
+/* Add Github CSS  */
+
+var Tag_CSSAdded = false;
+var Attribute_CSSAdded = false;
+
+function addCSSforTag(fileName) {
+	if (Tag_CSSAdded == false) {
+		DEBUG("Added CSS for Tag(s)")
+		var head = document.head;
+		var link = document.createElement("link");
+
+		link.type = "text/css";
+		link.rel = "stylesheet";
+		link.href = fileName;
+
+		head.appendChild(link);
+		Tag_CSSAdded = true
+
+	}
+
+
+
 }
 
-  
-  /// Markdown Parser Load Successful 
-script.onload = function () {
- 
-  // Let the Magic Begin 
+function addCSSforAttributes(fileName) {
+	if (Attribute_CSSAdded == false) {
+		DEBUG("Added CSS for Attribute(s)")
+		var head = document.head;
+		var link = document.createElement("link");
 
-renderMarkdown()
-  
-};
+		link.type = "text/css";
+		link.rel = "stylesheet";
+		link.href = fileName;
+
+		head.appendChild(link);
+		Attribute_CSSAdded = true
+
+	}
+}
+
+
+function markdownToHTML(tags, flavor, isAttribute) {
+	DEBUG("Converting Markdown To HTML")
+	if (flavor === "GFM") {
+		// if attribute type - it requires a class added. 
+		var converter = new showdown.Converter()
+
+		if (isAttribute == true) {
+			tags.forEach(tag => {
+				tag.classList.add("github-md");
+			})
+
+			// Stylesheet for attributes
+			addCSSforAttributes('https://cdn.jsdelivr.net/gh/MarketingPipeline/Markdown-Elements/stylesheets/github_md_attr.min.css');
+
+		} else {
+
+			// Stylesheet for tags
+			addCSSforTag('https://cdn.jsdelivr.net/gh/MarketingPipeline/Markdown-Tag/stylesheets/github_md.min.css');
+
+		}
+
+		converter.setOption('tables', 'on')
+
+		converter.setOption('emoji', 'on')
+
+		converter.setOption('strikethrough', 'on')
+
+		converter.setOption('tasklists', 'true')
+
+
+		converter.setOption('ghMentions', 'true')
+
+
+		converter.setOption('simplifiedAutoLink', 'true');
+	} else {
+
+		var converter = new showdown.Converter()
+		converter.setOption('tables', 'on')
+
+		converter.setOption('emoji', 'on')
+
+		converter.setOption('strikethrough', 'on');
+
+	}
+
+	tags.forEach(tag => {
+		if (tag.getAttribute("src")) {
+			// load file / url content
+			getData(tag.getAttribute("src")).then(data => {
+				if (data instanceof Error) {
+					// error fetching file
+					tag.innerHTML = converter.makeHtml("Error rendering file to markdown")
+				} else {
+
+					// there was NOT an error fetching file / URL content
+					tag.innerHTML = converter.makeHtml(data)
+				}
+			})
+
+		}
+		// Tag does NOT have src attribute
+		else {
+			// render markdown content in md tag or attribute
+
+			tag.innerHTML = converter.makeHtml(tag.innerHTML.replace(/&gt;/g, '>'))
+		}
+
+	});
+}
+
+
+
+
+
+function renderMarkdown() {
+
+
+	/* Convert Markdown Attributes */
+
+
+
+	if (document.querySelectorAll('[md]').length > 0) {
+
+		DEBUG("MD attribute(s) were found, coverting content to Markdown")
+		MD_TAGs = document.querySelectorAll('[md]');
+
+
+		markdownToHTML(MD_TAGs, MarkdownFlavor = null, isAttribute = true)
+
+
+
+
+	}
+
+
+
+
+	if (document.querySelectorAll('[github-md]').length > 0) {
+
+		DEBUG("github-md attribute(s) were found, coverting content to Markdown")
+
+		GitHub_MD_TAGs = document.querySelectorAll('[github-md]');
+
+
+		markdownToHTML(GitHub_MD_TAGs, MarkdownFlavor = "GFM", isAttribute = true)
+
+	}
+
+
+	/* Convert Markdown Tags */
+
+	if (document.getElementsByTagName("md").length > 0) {
+		DEBUG("md tag(s) were found, coverting content to Markdown")
+
+		MDTAGs = document.querySelectorAll('md');
+
+
+		markdownToHTML(MDTAGs, MarkdownFlavor = null, isAttribute = false)
+
+
+
+
+	}
+
+
+
+	if (document.getElementsByTagName("github-md").length > 0) {
+		DEBUG("github-md tag(s) were found, coverting content to Markdown")
+
+		GitHub_MD_TAGs = document.querySelectorAll("github-md");
+
+
+
+		markdownToHTML(GitHub_MD_TAGs, MarkdownFlavor = "GFM", isAttribute = false)
+	}
+}
+
+
+
+
+
+
+function loadMarkdownParser() {
+	DEBUG("Adding Markdown Parser To HTML document")
+	/// Add Markdown Parser To Document
+	var script = document.createElement('script');
+	script.src = "https://cdn.jsdelivr.net/gh/MarketingPipeline/Markdown-Tag/parsers/showdown.min.js";
+
+	document.head.appendChild(script); //or something of the likes  
+
+
+
+
+	// On Error Loading Markdown Parser
+	script.onerror = function() {
+
+		console.error("Markdown Tag: Error while performing function LoadMarkdownParser - There was an error loading the Markdown Parser")
+
+	}
+
+
+	/// Markdown Parser Load Successful 
+	script.onload = function() {
+
+		// Let the Magic Begin 
+		DEBUG("Markdown Parser Load Successful")
+		renderMarkdown()
+
+	};
 }
 loadMarkdownParser()
